1b173fe532ccd1a87de01acf2fe9279e4eca7a3a
infinit7Even <infinit7even@gmail.com>
2020-12-16 14:26:42 +0100

    Nuove Emoji

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,17 +1,17 @@
 module.exports = {
     name: 'queue',
-    aliases: [],
+    aliases: ["coda"],
     category: 'Music',
     utilisation: '{prefix}queue',
 
     execute(client, message) {
-        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
+        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - No sei in un canale vocale!`);
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
 
         const queue = client.player.getQueue(message);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No songs currently playing !`);
+        if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - Non ci sono canzoni in coda!`);
 
         message.channel.send(`**Server queue - ${message.guild.name} ${client.emotes.queue}**\nCurrent : ${queue.playing.title} | ${queue.playing.author}\n\n` + (queue.tracks.map((track, i) => {
             return `**#${i + 1}** - ${track.title} | ${track.author} (requested by : ${track.requestedBy.username})`
