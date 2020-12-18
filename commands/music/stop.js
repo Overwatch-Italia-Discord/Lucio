78c97eb933abd627d53487f18d369654e4d6bb1b
ssebastianoo <sebastianogirotto05@gmail.com>
2020-12-18 10:26:56 +0100

    📑 Translations

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
     name: 'stop',
     aliases: [],
@@ -5,15 +7,27 @@ module.exports = {
     utilisation: '{prefix}stop',
 
     execute(client, message) {
-        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - Devi **essere** in un **canale vocale** per poter **utilizzare** il Bot!`);
+        const emb = new Discord.MessageEmbed()
+        .setColor('#fa9c1e')
+        if (!message.member.voice.channel) {
+            emb.setDescription(`${client.emotes.error} Devi essere in un **canale vocale** per poter **utilizzare** questo comando!`)
+            return message.channel.send(emb)
+        }
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - Devi **essere** in un **canale vocale** per poter **utilizzare** il Bot!`);
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+            emb.setDescription(`${client.emotes.error} Devi essere nel mio stesso **canale vocale** per poter **utilizzare** questo comando!`)
+            return message.channel.send(emb)
+        }
 
-        if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - **Nessun **brano **attualmente **in **riproduzione**`);
+        if (!client.player.getQueue(message)) {
+            emb.setDescription(`${client.emotes.error} **Nessun** brano **attualmente** in **riproduzione**.`)
+            return message.channel.send(emb)
+        }
 
         client.player.setRepeatMode(message, false);
         client.player.stop(message);
-
-        message.channel.send(`${client.emotes.success} - Ho **interrotto** la **riproduzione**`);
+        
+        emb.setDescription(`${client.emotes.success} Ho **interrotto** la **riproduzione**`)
+        message.channel.send(emb);
     },
 };
