32ab24994cba4f75879fc43f6d71c98273bf9b02
ssebastianoo <sebastianogirotto05@gmail.com>
2020-12-18 10:27:42 +0100

    🌀 Command Alias

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'stop',
-    aliases: [],
+    aliases: ["ferma", "fermo", "dc", "disconnect", "leave"],
     category: 'Music',
     utilisation: '{prefix}stop',
 
