3d3f6f21edaf2d047f6c48341b8e5d8ff5f8056c
ssebastianoo <sebastianogirotto05@gmail.com>
2020-12-18 10:22:08 +0100

    🌀 Command Alias

--- commands/music/filter.js
+++ commands/music/filter.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'effetto',
-    aliases: [],
+    aliases: ['filter'],
     category: 'Music',
     utilisation: '{prefix}filter [filter name]',
 
