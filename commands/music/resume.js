82fa5ebee35fedc28d917e89ccab47c7c96bb565
ssebastianoo <sebastianogirotto05@gmail.com>
2020-12-18 10:20:37 +0100

    🌀 Command Renamed

--- commands/music/resume.js
+++ commands/music/resume.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports = {
-    name: 'ripreso',
+    name: 'riprendi',
     aliases: ['resume'],
     category: 'Music',
     utilisation: '{prefix}resume',
